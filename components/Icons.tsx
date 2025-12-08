bda0775a5d9993b12abd754b56f81a5edec0015c
David L Gilbert <dvdlgilbert@users.noreply.github.com>
2025-12-08 02:13:38 -0600

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import * as React from 'react';
 
 export const PlusIcon = ({ className = 'w-6 h-6' }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
