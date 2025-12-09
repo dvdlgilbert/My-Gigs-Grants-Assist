60439dba5732d8bdc7bd386985dc41885d5bd0be
David L Gilbert <dvdlgilbert@users.noreply.github.com>
2025-12-09 01:57:21 -0600

    Refactor useLocalStorage to use useState directly

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import { useState } from 'react';
 
 export function useLocalStorage<T,>(key: string, initialValue: T): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const [storedValue, setStoredValue] = React.useState<T>(() => {
+  const [storedValue, setStoredValue] = useState<T>(() => {
     try {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
