8bcba5b8ec3d85ad796b68423f43d1a732f7e09f
David L Gilbert <dvdlgilbert@users.noreply.github.com>
2025-12-09 14:10:35 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-primary': '#0284c7',
+        'brand-secondary': '#f0f9ff',
+        'brand-accent': '#0ea5e9',
+        'brand-dark': '#0c4a6e',
+      },
+    },
+  },
+  plugins: [],
+}
