72093d7b60256e0b40e8ed67265faede722f86ab
David L Gilbert <dvdlgilbert@users.noreply.github.com>
2025-12-09 01:54:30 -0600

    Refactor import statement for React

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+
+import React from 'react';
 
 export const PlusIcon = ({ className = 'w-6 h-6' }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
